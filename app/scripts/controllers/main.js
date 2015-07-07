a3bcec17b49ab47a9617d6466cc0f05b4ade05b3
Nelson Chan <khchanel@gmail.com>
2015-07-07 22:21:51 +1000

    UX improvement

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,44 @@
  * Controller of the seqcalcApp
  */
 angular.module('seqcalcApp')
-  .controller('MainCtrl', ['$scope', 'Sequencer', function ($scope, Sequencer) {
+  .controller('MainCtrl', function ($scope, Sequencer, $window, $timeout) {
     $scope.seqnumber = null;
     $scope.isPlainText = false;
 
-    $scope.$watch('seqnumber', function() {
+    /* compute sequences */
+    $scope.compute = function() {
+      if ($scope.seqnumber > 1000 &&
+        !$window.confirm($scope.seqnumber + " is a bit large and may take a while, confirm?"))
+      {
+        // abort & reset
+        $scope.seqnumber = null;
+        return;
+      }
+
       $scope.allNumbers = Sequencer.all($scope.seqnumber);
       $scope.oddNumbers = Sequencer.odd($scope.seqnumber);
       $scope.evenNumbers = Sequencer.even($scope.seqnumber);
       $scope.except = Sequencer.except($scope.seqnumber);
       $scope.fibonacciNumbers = Sequencer.fibonacci($scope.seqnumber);
+    };
+
+    /* delay computation after input */
+    var filterTextTimeout;
+    $scope.$watch('seqnumber', function(newVal, oldVal) {
+      // skip no change
+      if (newVal === oldVal) {
+        return;
+      }
+
+      // cancel previous timeout promise
+      if (filterTextTimeout) {
+        $timeout.cancel(filterTextTimeout);
+      }
+
+      // make timeout action
+      filterTextTimeout = $timeout(function() {
+        $scope.compute();
+      }, 200); // delay in ms
     });
-  }]);
+
+  });
