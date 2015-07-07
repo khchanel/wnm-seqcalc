1789d7969489f3c1fd48c3ee770f5e5f0d2da2b5
Nelson Chan <khchanel@gmail.com>
2015-07-07 17:11:24 +1000

    scaffold web app using yeoman w/ generator-angular

---
+++ src/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seqcalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the seqcalcApp
+ */
+angular.module('seqcalcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
